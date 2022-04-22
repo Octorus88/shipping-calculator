a275ff95c0ffccf0c4627b7e38957dec8eaa6a82
Octorus88 <ninety_shock_00@icloud.com>
2022-04-22 14:42:00 +0300

    - Рефакторинг кода - Переписал логику расчета коэф. доставки

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
-
-
-def calc_shipping(distance, size, breakable, workload):
-    """Задача. Вам нужно написать функцию расчёта стоимости доставки.
-
-    Стоимость рассчитывается в зависимости от:
-
-    *расстояния до пункта назначения:*
-
-    - более 30 км: +300 рублей к доставке;
-    - до 30 км: +200 рублей к доставке;
-    - до 10 км: +100 рублей к доставке;
-    - до 2 км: +50 рублей к доставке;
-
-    *габаритов груза:*
-
-    - большие габариты: +200 рублей к доставке;
-    - маленькие габариты: +100 рублей к доставке;
-
-    *хрупкости груза.* Если груз хрупкий — +300 рублей к доставке. Хрупкие грузы нельзя возить на расстояние более 30 км;
-
-    *загруженности службы доставки*. Стоимость умножается на коэффициент доставки:
-
-    - очень высокая загруженность — 1.6;
-    - высокая загруженность — 1.4;
-    - повышенная загруженность — 1.2;
-    - во всех остальных случаях коэффициент равен 1.
-
-    Минимальная сумма доставки — 400 рублей. Если сумма доставки меньше минимальной, выводится минимальная сумма.
-
-    На входе функция получает расстояние до пункта назначения, габариты, информацию о
-    хрупкости, загруженность службы на текущий момент. На выходе пользователь получает стоимость доставки.
+def calc_shipping_cost(distance: int, size: str, breakable: bool, workload: str):
+    """
+    :param distance: расстояние до пункта назначения
+    :param size: габариты груза
+    :param breakable: хрупкость груза
+    :param workload: загруженность службы доставки
+    :return: shipping_cost: стоимость доставки
     """
     shipping_cost = 0
     if distance > 30:
@@ -48,17 +23,18 @@ def calc_shipping(distance, size, breakable, workload):
         if distance > 30:
             exit('Хрупкие грузы нельзя возить на расстояние более 30 км')
         shipping_cost += 300
-    workload_rates = {'very high': 1.6, 'high': 1.4, 'increased': 1.2, 'other': 1}
-    if workload not in workload_rates:
-        workload = 'other'
-    delivery_coefficient = workload_rates[workload]
+    workload_rates = {'very high': 1.6, 'high': 1.4, 'increased': 1.2}
+    try:
+        delivery_coefficient = workload_rates[workload]
+    except KeyError:
+        delivery_coefficient = 1
     shipping_cost *= delivery_coefficient
-    minimal_shipping_cost = 400
-    if shipping_cost < minimal_shipping_cost:
+    min_shipping_cost = 400
+    if shipping_cost < min_shipping_cost:
         print('Минимальная сумма доставки - 400 рублей.')
-    return f'Стоимость доставки - {round(shipping_cost)} рублей.'
+    return shipping_cost
 
 
 if __name__ == '__main__':
-    shipping = calc_shipping(distance=40, size='small', breakable=False, workload='highly')
+    shipping = calc_shipping_cost(distance=40, size='small', breakable=False, workload='highly')
     print(shipping)
